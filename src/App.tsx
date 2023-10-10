17fbc6df9458c2661c5283172b7c30c7664da5d5
Ayman Baioumy <abaioumy@e2r12p2.1337.ma>
2023-10-10 17:10:21 +0100

    styling is close to being finished

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,6 @@ interface Note {
   selected: boolean;
 }
 
-const sideBar_css: string = 'static bg-MidGray h-screen w-64 z-0 fixed top-0 left-26 rounded';
-const addButton_css: string = 'static bg-WhitishYellow text-black z-10 absolute bottom-5 left-10 rounded text-xl w-40 h-10 flex items-center justify-center';
-const confirmButton_css: string = 'static bg-WhitishYellow text-black z-10 rounded';
-const noteButtons_css: string = 'static bg-WhitishYellow text-black z-10 rounded';
-const noteContent_css: string = 'text-black';
-
 function App() {
   const [notes, setNotes] = useState<Note[]>(Array(0));
   // const [pressedNote, setPressedNote] = useState<boolean[]>(Array(false));
@@ -46,16 +40,20 @@ function App() {
     
     return (
       <div>
-            <h2>{n.title}</h2>
+            <h2 className='absolute top-10 left-80 text-4xl font-bold'>{n.title}</h2>
+            <div className='absolute top-20 left-80 w-40 border-b-2 border-black'></div>
             <textarea 
+            className='absolute grow top-40 left-80 w-80 h-80 text-black z-10 border-hidden outline-none rounded'
               id='text'
               placeholder="Write your note..."
+              cols={100}
+              rows={100}
               ref={textAreaRef}
               ></textarea>
-              <button className='confirm' onClick={() =>
+              <button className='' onClick={() =>
               {
                 UpdateContent(n.id);
-              }}>c</button>
+              }}>save</button>
           </div>
         );
   }
@@ -98,13 +96,13 @@ function App() {
   }
   //generates buttons for the existed notes
   function NoteButtons() {
-    return notes.map( note => (
-      <div className='row'>
-        <button className={noteButtons_css} onClick={ () => {
+    return (
+      notes.map( note => (
+        <button className='bg-blue w-60 h-10 left-2 text-white font-semibold z-10 rounded' onClick={ () => {
           HandleClick(note.id);
         }}>{note.title}</button>
-      </div>
-  ))
+      ))
+    )
   };
 
   //checks which note is selected and returns it to show its content
@@ -120,36 +118,72 @@ function App() {
     return (notes[0]);
   }
 
-  return (
-    <div>
-      <div className={sideBar_css}></div>
-      <div className='note list'>
-        {NoteButtons()}
-      </div>
-      <div>
-      { notes.length > 0 ? (<div className='content'>
-        {
-          <NotePage  {...SelectNote()} />
-        }
-      </div >) : (
-        <div className='absolute top-20 right-40 left-85 text-xl' ><p>no notes created</p></div>
-        )}
-      </div>
-      {showPopUp && (
-        <div className='popup'>
-          <input
-          type='text'
-          placeholder='enter title'
-          value={newNoteTitle}
-          onChange={(e) => setNoteTitle(e.target.value)}
-          ></input>
-          <button className={confirmButton_css} onClick={ConfirmTitle}>Confirm</button>
-          <button className={confirmButton_css} onClick={() => setPopUp(false)}>cancel</button>
-        </div>
-      )
+  {/* <div className='flex flex-col space-y-3 bg-MidGray h-screen w-64 z-0 fixed top-0 left-26 rounded'> */}
+      {NoteButtons()}
+    {/* <div>
+    <div className='absolute bg-WhitishYellow font-semibold text-black z-10 absolute bottom-5 left-10 rounded text-xl w-40 h-10 flex items-center justify-center' onClick={AddNote}>
+      <button>Add Note</button>
+    </div>
+  </div>
+  <div className='flex flex-col bg-pink'>
+    { notes.length > 0 ? (<div>
+      {
+        <NotePage  {...SelectNote()} />
       }
-      <div className={addButton_css} onClick={AddNote}>
-        <button>Add Note</button>
+    </div >) : (
+      <div className='text-xl' ><p>no notes created</p></div>
+      )}
+    {showPopUp && (
+      <div className='absolute w-64 h-20 bg-MidGray rounded'>
+        <input
+        className='relative bg-white left-5 top-5 text-black z-10 rounded'
+        type='text'
+        placeholder='enter title'
+        value={newNoteTitle}
+        onChange={(e) => setNoteTitle(e.target.value)}
+        ></input>
+        <button className='absolute left-1 bottom-1 bg-WhitishYellow w-20 text-black z-10 rounded' onClick={ConfirmTitle}>Confirm</button>
+        <button className='absolute right-1 bottom-1 bg-WhitishYellow w-20 text-black z-10 rounded' onClick={() => setPopUp(false)}>Cancel</button>
+      </div>
+  )
+}
+    </div> */}
+  return (
+    <div className='flex flex-row h-full w-full bg-pink'>
+      {/* sidebar */}
+      <div className='flex-col w-64 bg-MidGray h-screen rounded-r-2xl'>
+        <div>{NoteButtons()}</div>
+        <div className='bg-WhitishYellow w-40 text-center rounded ml-5 self-baseline ' onClick={AddNote}><button>Add Note</button></div>
+      </div>
+      {/* note content */}
+      <div className='bg-pink w-full flex-col m-6'>
+          { notes.length > 0 ? (<div>
+          {
+            <NotePage  {...SelectNote()} />
+          }
+          </div >) : (
+          <div className='text-xl ml-80' ><p>no notes created</p></div>
+          )}
+          {showPopUp && (
+          <div className='flex flex-col ml-60 w-64 h-20 bg-MidGray rounded'>
+            <div className='mt-2 ml-6'>
+            <input
+            className='bg-white text-black z-10 rounded'
+            type='text'
+            placeholder='enter title'
+            value={newNoteTitle}
+            onChange={(e) => setNoteTitle(e.target.value)}
+            ></input>
+            </div>
+            <div className='flex flex-row'>
+              <div><button className='ml-5 mt-5 bg-WhitishYellow w-20 text-black z-10 rounded' onClick={ConfirmTitle}>Confirm</button></div>
+              <div><button className='ml-16 mt-5 bg-WhitishYellow w-20 text-black z-10 rounded' onClick={() => setPopUp(false)}>Cancel</button></div>
+            </div>
+          </div>
+          )}
+        {/* <div className='w-full h-14 border-b-solid border-black border-b-[2px] bg-white text-3xl'>title</div>
+        <div className='bg-white mt-4'>asdkjasdluasdkjahsdasd
+        asdjasdkahsdashd asjdhaskjdhaskljdhas dasjdhas dd </div> */}
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@ module.exports = {
   content: ["./src/*.tsx"],
   theme: {
     colors: {
-      'MidGray': '#555b6e',
-      'pink': '#ab899f',
+      'MidGray': '#555B6E',
+      'pink': '#D3C0CD',
       'hover_pink': '#936f84',
       'white': '#ffffff',
       'black': '#000000',
       'WhitishYellow': '#FFD6BA',
+      'blue': '#636A81',
     },
     extend: {},
   },
