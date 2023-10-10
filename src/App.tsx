2a7d97b0ba9451c398b61243a4d7b6977a5556ab
Ayman Baioumy <abaioumy@e2r12p2.1337.ma>
2023-10-10 19:06:34 +0100

    just needs some final touches

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import { useState } from 'react';
-import { text } from 'stream/consumers';
 import './index.css'
 
 interface Note {
@@ -40,17 +39,17 @@ function App() {
     
     return (
       <div>
-            <h2 className='absolute top-10 left-80 text-4xl font-bold'>{n.title}</h2>
-            <div className='absolute top-20 left-80 w-40 border-b-2 border-black'></div>
+            <h2 className='ml-6 text-4xl font-bold'>{n.title}</h2>
+            <div className='mt-2 w-full border-b-2 border-black'></div>
             <textarea 
-            className='absolute grow top-40 left-80 w-80 h-80 text-black z-10 border-hidden outline-none rounded'
+            className='mt-5 bg-pink w-full h-80 text-black z-10 border-hidden rounded'
               id='text'
               placeholder="Write your note..."
               cols={100}
               rows={100}
               ref={textAreaRef}
               ></textarea>
-              <button className='' onClick={() =>
+              <button className='bg-WhitishYellow rounded w-20 text-center font-semibold' onClick={() =>
               {
                 UpdateContent(n.id);
               }}>save</button>
@@ -86,7 +85,7 @@ function App() {
 
   function HandleClick(id: number) {
     let updatedNotes = [...notes];
-    updatedNotes.map( (note) => {
+    updatedNotes.forEach( (note) => {
       if (note.id === id)
         note.selected = true;
       else
@@ -98,7 +97,7 @@ function App() {
   function NoteButtons() {
     return (
       notes.map( note => (
-        <button className='bg-blue w-60 h-10 left-2 text-white font-semibold z-10 rounded' onClick={ () => {
+        <button className='bg-blue ml-5 w-40 h-10 text-white rounded' onClick={ () => {
           HandleClick(note.id);
         }}>{note.title}</button>
       ))
@@ -117,43 +116,13 @@ function App() {
     };
     return (notes[0]);
   }
-
-  {/* <div className='flex flex-col space-y-3 bg-MidGray h-screen w-64 z-0 fixed top-0 left-26 rounded'> */}
-      {NoteButtons()}
-    {/* <div>
-    <div className='absolute bg-WhitishYellow font-semibold text-black z-10 absolute bottom-5 left-10 rounded text-xl w-40 h-10 flex items-center justify-center' onClick={AddNote}>
-      <button>Add Note</button>
-    </div>
-  </div>
-  <div className='flex flex-col bg-pink'>
-    { notes.length > 0 ? (<div>
-      {
-        <NotePage  {...SelectNote()} />
-      }
-    </div >) : (
-      <div className='text-xl' ><p>no notes created</p></div>
-      )}
-    {showPopUp && (
-      <div className='absolute w-64 h-20 bg-MidGray rounded'>
-        <input
-        className='relative bg-white left-5 top-5 text-black z-10 rounded'
-        type='text'
-        placeholder='enter title'
-        value={newNoteTitle}
-        onChange={(e) => setNoteTitle(e.target.value)}
-        ></input>
-        <button className='absolute left-1 bottom-1 bg-WhitishYellow w-20 text-black z-10 rounded' onClick={ConfirmTitle}>Confirm</button>
-        <button className='absolute right-1 bottom-1 bg-WhitishYellow w-20 text-black z-10 rounded' onClick={() => setPopUp(false)}>Cancel</button>
-      </div>
-  )
-}
-    </div> */}
   return (
     <div className='flex flex-row h-full w-full bg-pink'>
       {/* sidebar */}
-      <div className='flex-col w-64 bg-MidGray h-screen rounded-r-2xl'>
-        <div>{NoteButtons()}</div>
-        <div className='bg-WhitishYellow w-40 text-center rounded ml-5 self-baseline ' onClick={AddNote}><button>Add Note</button></div>
+      <div className='flex flex-col space-y-3 w-64 bg-MidGray h-screen rounded-r-2xl'>
+        <div className='text-4xl text-WhitishYellow ml-4 mt-4 [text-shadow:_0_1px_0_rgb(0_0_0_/_60%)]'><h1>MyNotes</h1></div>
+        <div className='mt-40'>{NoteButtons()}</div>
+        <div className='absolute bg-WhitishYellow w-40 text-center rounded bottom-4 ml-5 self-baseline font-bold' onClick={AddNote}><button>Add Note</button></div>
       </div>
       {/* note content */}
       <div className='bg-pink w-full flex-col m-6'>
@@ -181,9 +150,6 @@ function App() {
             </div>
           </div>
           )}
-        {/* <div className='w-full h-14 border-b-solid border-black border-b-[2px] bg-white text-3xl'>title</div>
-        <div className='bg-white mt-4'>asdkjasdluasdkjahsdasd
-        asdjasdkahsdashd asjdhaskjdhaskljdhas dasjdhas dd </div> */}
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ],
 }
 
